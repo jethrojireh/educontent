dc03997166c9e84ba8b61ae8196af999f17f1129
Jethro Jireh D. Tolosa <jethro@levitansoftware.net>
2013-09-06 09:45:13 +0800

    added new php scripts for populating dropdown and preview ing flashcard content tab in front end

---
+++ admin/templates/preview.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+$id = $_GET['id'];
+$db = new mysqli('localhost','levitan5_webdev','xR4OfBo41rzm','levitan5_esisswp');
+$query1 = "SELECT id FROM wp_title WHERE title ='" . $id . "'";
+$results = $db->query($query1);
+
+while($rows = $results->fetch_assoc()){
+    $var = $rows['id'];
+}
+
+
+$query = "SELECT id, word FROM wp_word WHERE title_id ='" . $var . "'";
+$result = $db->query($query);
+
+while($row = $result->fetch_assoc()){
+        //echo "<option>" .$row['word']. "</option>";
+        echo '
+                <input type="checkbox">
+                '.$row['word'].'
+                <br/>';
+      }
+
+      
+mysqli_close($db);
+
+
+?>
---
+++ admin/templates/subcateg.php
@@ -0,0 +1,22 @@
+<?php
+$id = $_GET['id'];
+$db = new mysqli('localhost','levitan5_webdev','xR4OfBo41rzm','levitan5_esisswp');
+$query = "SELECT id,title FROM wp_title where category_id ='" . $id . "'";
+$result = $db->query($query);
+
+echo "<select>";
+echo"<option>Select Title...</option>";
+while($row = $result->fetch_assoc()){
+    
+        
+        echo "<option>" .$row['title']. "</option>";
+       
+        
+      }
+   
+    echo "</select>";  
+      
+mysqli_close($db);
+
+
+?>
